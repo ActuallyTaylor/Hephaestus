567e5336735b21189c9663bf1bd25ec85c796bdf
Zachary Lineman <zachary.lineman@gmail.com>
2021-10-01 12:00:58 -0400

    Updated Shader Class

--- Hephaestus-C/Hephaestus/src/Classes/Shader/Shader.hpp
+++ Hephaestus-C/Hephaestus/src/Classes/Shader/Shader.hpp
@@ -17,6 +17,8 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include "glm.hpp"
+#include "type_ptr.hpp"
 
 using namespace std;
 
@@ -30,6 +32,17 @@ public:
     GLuint shaderProgram;
     void setup ();
     void use();
+    
+    void setFloat (const char *name, float value, bool useShader = false);
+    void setInteger (const char *name, int value, bool useShader = false);
+    void setVector2f (const char *name, float x, float y, bool useShader = false);
+    void setVector2f (const char *name, const glm::vec2 &value, bool useShader = false);
+    void setVector3f (const char *name, float x, float y, float z, bool useShader = false);
+    void setVector3f (const char *name, const glm::vec3 &value, bool useShader = false);
+    void setVector4f (const char *name, float x, float y, float z, float w, bool useShader = false);
+    void setVector4f (const char *name, const glm::vec4 &value, bool useShader = false);
+    void setMatrix4 (const char *name, const glm::mat4 &matrix, bool useShader = false);
+
 private:
 
 };
