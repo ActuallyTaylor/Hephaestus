13de2355394abf9ddbdec23c9debe5f43f9744d2
Zachary Lineman <zachary.lineman@gmail.com>
2021-10-26 21:33:15 -0400

    Trying to find a new build enviroment.

---
+++ Clion/Hephaestus/cmake-build-debug/CMakeFiles/Hephaestus.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hephaestus.dir/API/Hephaestus.cpp.o"
+  "CMakeFiles/Hephaestus.dir/main.cpp.o"
+  "Hephaestus"
+  "Hephaestus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hephaestus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Engine/API/Hephaestus.hpp
@@ -0,0 +1,27 @@
+/*
+    Hephaestus.hpp
+    Zachary Lineman
+    October 26th 2021
+
+    =================
+    DESCRIPTION
+    =================
+*/
+
+#ifndef Hephaestus_hpp
+#define Hephaestus_hpp
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW & GLFW headers
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+class Hephaestus
+{
+    Hephaestus();
+};
+
+#endif
