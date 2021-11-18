7d359fea87faecde9c10454d3976e33177e15690
Zachary Lineman <zachary.lineman@gmail.com>
2021-11-18 00:06:42 -0500

    Collision but it creates energy on accident

--- Olympus/Hephaestus/Window/Sprite/PhysicsSprite/Collision/Collision.cpp
+++ Olympus/Hephaestus/Window/Sprite/PhysicsSprite/Collision/Collision.cpp
@@ -33,10 +33,25 @@ void Collision::perform() {
     glm::vec3 v1 = one->velocity;
     glm::vec3 v2 = two->velocity;
 
+    glm::vec3 p1 = one->position;
+    glm::vec3 p2 = two->position;
+
+    float dotProduct = glm::dot(v1 - v2, p1 - p2);
+    float lengthS = pow(glm::length(p1 - p2), 2);
+
+    glm::vec3 vf1 = v1 - ( 2 * m2 / m1 + m2 ) * (dotProduct / lengthS ) * ( p1 - p2 );
+
+    dotProduct = glm::dot(v2 - v1, p2 - p1);
+    lengthS = pow(glm::length(p2 - p1), 2);
+    glm::vec3 vf2 = v2 - ( 2 * m1 / m1 + m2 ) * (dotProduct / lengthS ) * ( p2 - p1 );
     // Take the normal vector and multiply it by the penetration to find the angle that we need to offset the spheres so that they do not collide
     glm::vec3 positionOffset = normal * penetration;
 
-    one->position -= normal * penetration;
-    two->position += normal * penetration;
+    one->position -= positionOffset;
+    two->position += positionOffset;
+
+    one->velocity += vf1 * normal;
+    two->velocity += vf2 * normal;
+    printf("Velocities: %s, %s\n", glm::to_string(one->getVelocity()).c_str(), glm::to_string(two->getVelocity()).c_str());
 
 }
