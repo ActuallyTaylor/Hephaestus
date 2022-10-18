db337b79efac9906468d98ff326121fd9932e0c7
Zachary Lineman <zachary.lineman@gmail.com>
2022-10-18 00:44:38 -0400

    Create installDependencies.sh

---
+++ installDependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt install -y software-properties-common
+sudo apt update --allow-releaseinfo-change
+sudo add-apt-repository ppa:george-edison55/cmake-3.x
+sudo apt -y install build-essential
+sudo apt update --allow-releaseinfo-change
+sudo apt -y install cmake
+sudo apt -y install libglfw3
+sudo apt -y install libglfw3-dev
+sudo apt -y install libglew-dev
+sudo apt -y install libfreetype6-dev
+sudo apt -y install libglm-dev
+git clone https://github.com/ActuallyZach/Hephaestus.git
