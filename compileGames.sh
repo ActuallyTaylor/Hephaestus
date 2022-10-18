e3389f95a81fa33abfe81f86a5948374e672289a
Zachary Lineman <zachary.lineman@gmail.com>
2022-10-18 00:56:25 -0400

    Update compileGames.sh

--- compileGames.sh
+++ compileGames.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 CWD=$(pwd)
-echo "Writing in:"
-echo $CWD
+echo "Writing in: $CWD"
 
 rm -rf $CWD/compiled
 mkdir $CWD/compiled
@@ -12,20 +11,28 @@ mkdir $CWD/compiled/Olympus/
 
 cd $CWD
 cd ./compiled/GameOfLife/
+echo "CMAKE Game of Life $CWD"
 cmake $CWD/Games/GameOfLife/CMakeLists.txt
+echo "Compiling Game Of Life $CWD"
 make
 
 cd $CWD
 cd ./compiled/LowData/
+echo "CMAKE Low Data $CWD"
 cmake $CWD/Games/LowData/CMakeLists.txt
+echo "Compiling Low Data $CWD"
 make
 
 cd $CWD
 cd ./compiled/PhysicsDemo/
+echo "CMAKE Physics Demo $CWD"
 cmake $CWD/Games/PhysicsDemo/CMakeLists.txt
+echo "Compiling Physics Demo $CWD"
 make
 
 cd $CWD
 cd ./compiled/Olympus/
+echo "CMAKE Olympus $CWD"
 cmake $CWD/Olympus/CMakeLists.txt
+echo "Compiling Olympus $CWD"
 make
