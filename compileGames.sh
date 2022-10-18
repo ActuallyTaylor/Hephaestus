072ea9b3b213112813d57792d65eff4ca2d87a34
Zachary Lineman <zachary.lineman@gmail.com>
2022-10-18 01:06:25 -0400

    Update compileGames.sh

--- compileGames.sh
+++ compileGames.sh
@@ -12,20 +12,19 @@ mkdir $CWD/compiled/Olympus/
 echo "CMAKE Game of Life $CWD"
 cmake -B$CWD/compiled/GameOfLife/ -S$CWD/Games/GameOfLife/
 echo "Compiling Game Of Life $CWD"
-make
+make $CWD/compiled/GameOfLife/
 
 echo "CMAKE Low Data $CWD"
 cmake -B$CWD/compiled/LowData/ -S$CWD/Games/LowData/
 echo "Compiling Low Data $CWD"
-make
+make $CWD/compiled/LowData/
 
 echo "CMAKE Physics Demo $CWD"
 cmake -B$CWD/compiled/PhysicsDemo/ -S$CWD/Games/PhysicsDemo/
 echo "Compiling Physics Demo $CWD"
-make
+make $CWD/compiled/PhysicsDemo/
 
-cd $CWD/compiled/Olympus/
 echo "CMAKE Olympus $CWD"
 cmake -B$CWD/compiled/Olympus/ -S$CWD/Games/Olympus/
 echo "Compiling Olympus $CWD"
-make
+make $CWD/compiled/Olympus/
