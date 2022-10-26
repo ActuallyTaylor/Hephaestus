37636ac0ae143ca88fb1d88a2fbf9726d5889645
Zachary Lineman <zachary.lineman@gmail.com>
2022-10-26 01:40:23 -0400

    Trying more stuff

--- installDependencies.sh
+++ installDependencies.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-sudo apt install -y software-properties-common
-sudo apt update --allow-releaseinfo-change
-sudo add-apt-repository ppa:george-edison55/cmake-3.x
-sudo apt -y install build-essential
-sudo apt update --allow-releaseinfo-change
-sudo apt -y install cmake
-sudo apt -y install libglfw3
-sudo apt -y install libglfw3-dev
-sudo apt -y install libglew-dev
-sudo apt -y install libfreetype6-dev
-sudo apt -y install libglm-dev
-sudo apt -y install libalut-dev
-sudo apt-get install libopenal-dev
+sudo apt-get -y install build-essential
+sudo apt-get update --allow-releaseinfo-change
+sudo apt-get -y install cmake
+sudo apt-get -y install libglfw3
+sudo apt-get -y install libglfw3-dev
+sudo apt-get -y install libglew-dev
+sudo apt-get -y install libfreetype6-dev
+sudo apt-get -y install libglm-dev
+sudo apt-get -y install libalut-dev
+sudo apt-get -y install libopenal-dev
 git clone https://github.com/ActuallyZach/Hephaestus.git
+cd ./Hephaestus/
+./compileGames.sh
